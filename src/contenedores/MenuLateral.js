2cdeddac9d149465b4d2bbc207aff8a0e0b8a5cf
Angel Herrera Mendoza <anjelhm@gmail.com>
2017-10-11 16:01:36 -0500

    comunes

---
+++ src/contenedores/MenuLateral.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { View } from 'react-native';
+
+class MenuLateral extends Component {
+  render() {
+    return (
+      <View>
+      </View>
+    );
+  }
+}
+
+export default MenuLateral;
