f31f3c552c344a7c7f6ac3018c0c466f88c062d2
pedro sanchez <p03349@hotmail.com>
2017-10-11 17:51:39 -0500

    Menu en lado inferior

---
+++ src/componentes/Menu/Menubar.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import { View, StyleSheet } from 'react-native';
+class Menubar extends Component {
+
+renderTabs (){
+  const { children, activo }= this.props;
+  return React.Children.map(children,(child,index)=>{
+    return React.cloneElement(child,{
+      active: activo===index
+    })
+  })
+}
+
+  render() {
+
+    return (
+    <View style={ estilo.contenido }>
+    {this.renderTabs()}
+    </View>
+    );}
+}
+const estilo= StyleSheet.create({
+  contenido:{
+    height: 50,
+    flexDirection: 'row',
+    elevation: 4,
+    backgroundColor:"white",
+    justifyContent: 'space-around'
+  }
+});
+export default Menubar;
