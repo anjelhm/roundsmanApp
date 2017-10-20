e7830a970bcb1ae7eb5a8079ee55a14229abc624
pedro172023 <p03349@hotmail.com>
2017-10-20 16:32:35 -0500

    Agregando Registro final

--- src/componentes/Registro.js
+++ src/componentes/Registro.js
@@ -4,7 +4,8 @@ import {
   Text,
   TouchableOpacity,
   StyleSheet,
-  Picker
+  Picker,
+  ScrollView
 } from 'react-native';
 import DatePicker from 'react-native-datepicker';
 
@@ -15,19 +16,71 @@ class Registro extends Component {
 
   constructor() {
     super();
-
-    this.state = {
+        this.state = {
           date: '',
-          selected1: 'key1',
-          selected2: 'key1',
-          selected3: 'key1',
-          color: 'red',
-          mode: Picker.MODE_DIALOG,
+          sexo: '',
+          apellidop: '',
+          apellidom: '',
+          nombre: '',
+          clave1:'',
+          clave2:'',
+          correo: '',
+          estado: ''
     };
+  }
+  /**
+  * función que obtiene el valor de la tecla presionada y la asigna al value del input correspondiente
+  * @param { string } NombreInput
+  * @param { string } value
+  */
+  onChange(NombreInput, value) {
+    this.setState({
+      [NombreInput]: value
+    });
+  }
+  /**
+  * función que obtiene el valor de la tecla presionada y la asigna al value al input clave2
+  * @param { string } value
+  */
+  confirmar(value) {
+    this.setState({
+      clave2 : value
+  },this.fin.bind(this));
 
   }
-
-
+  /**
+  * función que compara la clave1 con la clave2 mientras las teclas en el input de clave2 se van presionando
+  */
+ fin(){
+   if(this.state.clave1===this.state.clave2){
+     this.setState({
+       estado: "true"
+       });
+   }else{
+     this.setState({
+       estado: "false"
+       });
+   }
+ }
+ /**
+ * función que envía el correo y la clave al contenedor IniciaSesion para verificar al usuario, y limpia los input
+ */
+ enviaDatos() {
+   if( this.state.estado==="true"){
+   this.props.enviaDatos(this.state.nombre, this.state.apellidom, this.state.apellidop, this.state.date, this.state.sexo, this.state.correo,  this.state.clave1);
+   this.setState({
+     date: '',
+     sexo: '',
+     apellidop: '',
+     apellidom: '',
+     nombre: '',
+     clave1:'',
+     clave2:'',
+     correo: '',
+     estado: ''
+   });
+ }
+ }
   render() {
 
     const highestTimeoutId = setTimeout(() => ';');
@@ -36,28 +89,55 @@ class Registro extends Component {
     }
 
     return (
-      <View>
+      <View >
+      <ScrollView>
+      <View style={styles.contenido} >
         <Input
           etiqueta = "Nombre"
           placeholder = "Ingresa aquí tu nombre"
+          onChange = { this.onChange.bind(this, 'nombre') }
+          value = { this.state.nombre }
+          width = { 300 }
         />
         <Input
-          etiqueta = "Sexo"
-          placeholder = "Ingresa tu sexo"
+          etiqueta = "Apellido Materno"
+          placeholder = "Ingresa aquí tu apellido materno"
+          onChange = { this.onChange.bind(this, 'apellidom') }
+          value = { this.state.apellidom }
+          width = { 300 }
         />
+        <Input
+          etiqueta = "Apellido Paterno"
+          placeholder = "Ingresa aquí tu apellido paterno"
+          onChange = { this.onChange.bind(this, 'apellidop') }
+          value = { this.state.apellidop }
+          width = { 300 }
+        />
+        <View style={{ width:300 }}>
+        <Text style = {{color: '#607D8B', width: '100%', fontSize: 15, justifyContent: 'center', alignItems: 'center' }}>Sexo</Text>
+        </View>
+
         <Picker
           style = { styles.picker }
-          selectedValue = { this.state.selected1 }
-        >
-          <Picker.Item label = "Mujer" value = "key0" />
-          <Picker.Item label = "Hombre" value = "key1" />
+          mode="dropdown"
+          itemStyle={{ fontSize: 25, color: 'red', textAlign: 'left', fontWeight: 'bold'}}
+          selectedValue = {this.state.sexo}
+          onValueChange = { this.onChange.bind(this, 'sexo') }>
+             <Picker.Item label = "Selecciona tu sexo" />
+             <Picker.Item label = "Hombre" value = "H" />
+             <Picker.Item label = "Mujer" value = "M" />
         </Picker>
         <Input
           etiqueta = "Correo Electrónico"
           placeholder = "Ingresa aquí tu correo"
           tipo = "email-address"
+          onChange = { this.onChange.bind(this, 'correo') }
+          value = { this.state.correo }
+          width = { 300 }
         />
-
+        <View style={{ width:300 }}>
+          <Text style = {{color: '#607D8B', width: '100%', fontSize: 15, justifyContent: 'center', alignItems: 'center' }}>Fecha de Nacimiento</Text>
+        </View>
         <DatePicker
           style = {{ width: 200 }}
           date = { this.state.date }
@@ -79,13 +159,32 @@ class Registro extends Component {
             }
             }}
           minuteInterval = { 10 }
-          onDateChange = { (date) => { this.setState({ date: date }); } }
+          onDateChange = { this.onChange.bind(this, 'date') }
         />
-        <View style = {{ marginTop: 10 }}>
+        <Input
+          etiqueta = "Contraseña"
+          placeholder = "Ingresa tu contraseña"
+          onChange = { this.onChange.bind(this, 'clave1') }
+          value = { this.state.clave1 }
+          password
+          width = { 300 }
+        />
+        <Input
+          etiqueta = "Confirmar Contraseña"
+          placeholder = "Ingresa tu contraseña"
+          onChange = { this.confirmar.bind(this) }
+          value = { this.state.clave2 }
+          password
+          width = { 300 }
+        />
+        <View style = {{ width: "100%", alignItems: 'center', justifyContent: 'center', marginTop: 10 }}>
           <Boton
              etiqueta = "Aceptar"
+             accion = { this.enviaDatos.bind(this) }
            />
         </View>
+        </View>
+       </ScrollView>
       </View>
     );
   }
@@ -94,6 +193,16 @@ const styles = StyleSheet.create({
   picker: {
     width: 300,
   },
+  text: {
+        fontSize: 30,
+        alignSelf: 'center',
+        color: 'red'
+     },
+ contenido: {
+       flex: 1,
+       alignItems: 'center',
+     }
+
 });
 
 export default Registro;
