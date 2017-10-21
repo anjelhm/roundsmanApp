e56ce457875aadcdb557f142d0ad8b2d53608b5a
Oscar Carrizal Martinez <ocarrizalmartinez@gmail.com>
2017-10-20 19:35:32 -0500

    Historial.js

---
+++ src/contenedores/Historial.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { View } from 'react-native';
+
+import MiHistorial from './componentes/Historial/MiHistorial';
+
+class Historial extends Component {
+  render() {
+    return(
+      <View style = {{ backgroundColor: '#CFD8DC'}}>
+        <MiHistorial/>
+      </View>
+    );
+  }
+}
+
+export default Historial;
