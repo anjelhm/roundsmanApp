7eaeb1ae7304f85abe59b66a076122fd26cc5f12
Oscar Carrizal Martinez <ocarrizalmartinez@gmail.com>
2017-10-24 15:24:46 -0500

    Modificando Barra_nav

--- src/comunes/Barra_nav.js
+++ src/comunes/Barra_nav.js
@@ -10,26 +10,33 @@ class Barra_nav extends Component {
 
     return (
       <View style = {{ height: 50, justifyContent: 'space-around', flexDirection: 'row', alignItems: 'center', backgroundColor: '#000000', elevation: 4, marginBottom: 10 }}>
-        <TouchableOpacity onPress = { retroceder }>
-          <Icon
-            name = "chevron-left"
-            size = { 40 }
-            color = "#FFFFFF"
-          />
-        </TouchableOpacity>
-        <Text style = {{ color: '#FFFFFF', fontSize: 28 }}>{ titulo }</Text>
-        {
-          boton
-          ? <TouchableOpacity
-              style = {{ height: 36, width: 62, justifyContent: 'center', alignItems: 'center', backgroundColor: '#FFFFFF', borderRadius: 5, elevation: 4 }}
-              onPress = { accion }
-            >
-              <Text style = {{ color: '#455A64', fontSize: 20 }}>
-                OK
-              </Text>
-            </TouchableOpacity>
-          : null
-        }
+        <View style = {{ flex: 1 }}>
+          <TouchableOpacity onPress = { retroceder }>
+            <Icon
+              name = "chevron-left"
+              size = { 40 }
+              color = "#FFFFFF"
+            />
+          </TouchableOpacity>
+        </View>
+        <View style = {{ flex: 3, justifyContent: 'center', alignItems: 'center' }}>
+          <Text style = {{ color: '#FFFFFF', fontSize: 28 }}>{ titulo }</Text>
+        </View>
+        <View style = {{ flex : 1 }}>
+          {
+            boton
+            ? <TouchableOpacity
+                style = {{ height: 36, width: 62, justifyContent: 'center', alignItems: 'center', backgroundColor: '#FFFFFF', borderRadius: 5, elevation: 4 }}
+                onPress = { accion }
+              >
+                <Text style = {{ color: '#455A64', fontSize: 20 }}>
+                  OK
+                </Text>
+              </TouchableOpacity>
+            : <View>
+              </View>
+          }
+        </View>
       </View>
     );
   }
