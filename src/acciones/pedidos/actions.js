3f3dc4966defcb3d1eb51978ce14117e6041c8d4
Oscar Carrizal Martinez <ocarrizalmartinez@gmail.com>
2017-10-26 17:57:32 -0500

    Acciones en pedidos/actions.js

---
+++ src/acciones/pedidos/actions.js
@@ -0,0 +1,30 @@
+import firebase, { firebaseRef } from '../../firebase';
+import {
+  OBTENER_PEDIDOS_INICIA,
+  OBTENER_PEDIDOS_OK,
+  OBTENER_PEDIDOS_ERROR
+} from '../../constantes/ActionTypes';
+
+export const obtenerPedidosInicia = () => ({
+  type: OBTENER_PEDIDOS_INICIA })
+export const obtenerPedidosOk = pedido => ({
+  type: OBTENER_PEDIDOS_OK, pedido })
+export const obtenerPedidosError = error =>
+  ({ type: OBTENER_PEDIDOS_ERROR, error });
+
+/**
+* funcion para obtener pedido
+**/
+export const iniciaObtenerPedido = () => {
+  return dispatch => {
+     dispatch(obtenerPedidosInicia());
+      /**
+      * funcion para obtener pedidos y envia
+      * @param { Object } pedidos
+      **/
+      firebaseRef.child('pedido').on('value', snapshot => {
+        dispatch(obtenerPedidosOk(snapshot.val()));
+      });
+    })
+  };
+};
