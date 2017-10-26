0457b354c971acf0a4d56fbc5193efc6a9c95062
Oscar Carrizal Martinez <ocarrizalmartinez@gmail.com>
2017-10-26 15:39:52 -0500

    :+1:  :+1:

--- src/acciones/repartidor/actions.js
+++ src/acciones/repartidor/actions.js
@@ -3,15 +3,13 @@ import {
   GUARDAR_REPARTIDOR_INICIA,
   GUARDAR_REPARTIDOR_OK,
   GUARDAR_REPARTIDOR_ERROR
-} from '../constantes/ActionTypes';
+} from '../../constantes/ActionTypes';
 
 export const guardarRepartidorInicia = () => ({
   type: GUARDAR_REPARTIDOR_INICIA
 })
 export const guardarRepartidorOk = payload => ({
-  type: GUARDAR_REPARTIDOR_OK,
-  payload
-})
+  type: GUARDAR_REPARTIDOR_OK, payload })
 export const guardarRepartidorError = error =>
   ({ type: GUARDAR_REPARTIDOR_ERROR, error });
 
