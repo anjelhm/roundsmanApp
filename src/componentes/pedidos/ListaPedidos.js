cda2f2a999b81f9520420f9c7dc84389657eeac3
gerardo <gerardking961231@gmail.com>
2017-10-26 23:12:18 -0500

    Pedidos

--- src/componentes/pedidos/ListaPedidos.js
+++ src/componentes/pedidos/ListaPedidos.js
@@ -1,25 +1,74 @@
 import React, { Component } from 'react';
-import { View, FlatList } from 'react-native';;
+import { View, Text, FlatList } from 'react-native';;
 
 //importar la clase TarjetaPedido
 import TarjetaPedido from './TarjetaPedido';
 
 class ListaPedidos extends Component {
+
+  constructor() {
+    super();
+
+    this.state = {
+      datos: [],
+      vacio: true
+    };
+
+  }
+
+  cargarDatos() {
+    if( typeof this.props.pedidos === 'undefined' ) {
+
+      this.setState({
+        datos: [],
+        vacio: true
+      });
+
+    } else {
+      this.setState({
+        datos: this.props.pedidos,
+        vacio: false
+      });
+    }
+  }
+
+  componentDidMount() {
+    this.cargarDatos();
+  }
+
+  componentDidUpdate(prevProps) {
+    if( prevProps.pedidos !== this.props.pedidos ) {
+      cargarDatos();
+    }
+  }
+
+  renderLista() {
+    return(
+      <FlatList
+        data = { this.state.datos }
+        renderItem = { ({ item }) => <TarjetaPedido
+                  nombre = { item.nombre }
+                  ubicacion = { item.ubicacion }
+                      /> }
+                  keyExtractor = { item => item.nombre }
+                  />
+    );
+  }
+
   render() {
     return(
-      <View>
-        <FlatList
-          data = { data }
-          renderItem = { ({ item }) => <TarjetaPedido
-                    nombre = { item.nombre }
-                    ubicacion = { item.ubicacion }
-                        /> }
-                    keyExtractor = { item => item.nombre }
-                    />
-                </View>
-              );
-            }
-          }
+      <View style = {{ flex: 1 }}>
+        {
+          !this.state.vacio ? this.renderLista() : (
+            <View style = {{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+              <Text>Cargando datos</Text>
+            </View>
+          )
+        }
+      </View>
+    );
+  }
+}
 
   const data = [
     {
--- src/contenedores/Pedidos.js
+++ src/contenedores/Pedidos.js
@@ -1,22 +1,55 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import { View, Text } from 'react-native';
+import { connect } from 'react-redux';
 
 import ListaPedidos from '../componentes/pedidos/ListaPedidos';
+import { obtenerPedidosInicia } from '../acciones/pedidos/actions'
 
 /**
  * cargando componente ListaPedidos
 */
 
-class Pedidos extends Component {
+class contenedorObtenerRegistro extends Component {
+
+  componentDidMount() {
+    this.props.obtenerPedidosInicia();
+  }
+
 
   render() {
 
+    const { pedidos } = this.props;
+
     return(
-      <View>
-        <ListaPedidos/>
+      <View style = {{ flex: 1 }}>
+        {
+          typeof pedidos.data === 'undefined'
+          ? <View style = {{ flex 1 }}> <Text>Obteniendo los pedidos</Text> </View>
+          : <View style = {{ flex: 1 }}>
+            {
+              pedidos.obteniendo
+              ? <View style = {{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+                <Text>Descargando pedidos</Text>
+              </View>
+              : <View style = {{ flex: 1 }}>
+                 <ListaPedidos pedidos = { pedidos.data }/>
+              </View>
+            }
+          </View>
+        }
       </View>
     );
 
   }
 }
-export default Pedidos;
+
+mapStateToProps = ({ pedidos: { pedidos } }) => ({
+  pedidos
+});
+
+ const ObtenerRegistro = connect (
+   mapStateToProps,
+   { obtenerPedidosInicia }
+ )( contenedorObtenerRegistro );
+
+export default ObtenerRegistro;
