b0613f1a80037388be12bd342736939bf104d3c6
pedro172023 <p03349@hotmail.com>
2017-10-28 18:31:30 -0500

    Estado del pedido

---
+++ src/acciones/estado/actions.js
@@ -0,0 +1,44 @@
+import firebase, { firebaseRef } from '../../firebase';
+import {
+        OBTENER_ESTADO_INICIA,
+        OBTENER_ESTADO_OK,
+        OBTENER_ESTADO_ERROR,
+        MODIFICAR_ESTADO_INICIA,
+        MODIFICAR_ESTADO_OK,
+        MODIFICAR_ESTADO_ERROR
+} from '../../constantes/ActionTypes';
+
+export const obtenerEstadoInicia = () => ({
+  type: OBTENER_ESTADO_INICIA });
+export const obtenerEstadoOk = estados => ({
+  type: OBTENER_ESTADO_OK, estados });
+export const obtenerEstadoError = error =>
+  ({ type: OBTENER_ESTADO_ERROR, error });
+
+  export const modificarEstadoInicia = () => ({
+    type: MODIFICAR_ESTADO_INICIA });
+  export const modificarEstadoOk = payload => ({
+    type: MODIFICAR_ESTADO_OK, payload });
+  export const modificarEstadoError = error =>
+    ({ type: MODIFICAR_ESTADO_ERROR, error });
+
+
+  export const iniciaObtenerEstado = (idUsario,idPedido) => {
+    return dispatch => {
+       dispatch(obtenerEstadoInicia());
+       firebaseRef.child(`usuarios/${idUsario}/pedidos/${idPedido}/estado`).on('value', snapshot => {
+         dispatch(obtenerEstadoOk( snapshot.val() ) );
+       });
+    };
+  };
+
+  export const iniciaCambiarEstado = (idUsario,idPedido,idRepartidor,idPedidoAceptado,estado) => {
+    return dispatch => {
+       dispatch(modificarEstadoInicia());
+       let modificarEstado = {};
+       modificarEstado[`usuarios/${idUsario}/pedidos/${idPedido}/estado`] = estado;
+       modificarEstado[`repartidor/${idRepartidor}/pedidos/${idPedidoAceptado}/estado`] = estado;
+       firebaseRef.update(modificarEstado)
+       .then( () => modificarEstadoOk("aceptado") )
+    };
+  };
